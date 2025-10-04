faa3842d166f49a76b2dbb11f0b7605a76a2b045
[dyad] <prakash081112@gmail.com>
2025-10-04 23:12:03 +0530

    [dyad] Fixed Supabase Edge Function deployment errors - wrote 1 file(s), renamed 1 file(s) + extra files edited outside of Dyad

--- supabase/functions/send-notification/index.ts
+++ supabase/functions/send-notification/index.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.190.0/http/server.ts";
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.45.0';
-import webpush from 'https://deno.land/x/webpush@1.0.0/mod.ts';
+import webpush from 'https://esm.sh/web-push@3.6.6'; // Changed import to esm.sh
 
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
