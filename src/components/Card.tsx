4b7f0a938b947f96d3426c05e58bd2cd7c1013b1
[dyad] <prakash081112@gmail.com>
2025-10-04 23:26:38 +0530

    [dyad] Added hover effect to cards. - wrote 1 file(s)

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import * as React from "react";
 
 import { cn } from "@/lib/utils";
@@ -9,7 +11,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-sm transition-all duration-200 ease-in-out hover:shadow-lg hover:-translate-y-1",
       className,
     )}
     {...props}
